049f7399b161f3fffbc793aafd19b123649fd121
shosseinimotlagh <shosseinimotlagh@ebay.com>
2024-03-01 13:47:55 -0800

    chaneg make

--- main.cpp
+++ main.cpp
@@ -1,30 +1,8 @@
-#include "gtest/gtest.h"
+#include <iostream>
 
-// Function to be tested
-int add(int a, int b) {
-    return a + b;
-}
 
-// Test suite
-class TestSuite : public ::testing::Test {
-protected:
-    void SetUp() override {
-        // Code here will be called immediately after the constructor (right
-        // before each test).
-    }
-
-    void TearDown() override {
-        // Code here will be called immediately after each test (right
-        // before the destructor).
-    }
-};
-
-// Test case
-TEST_F(TestSuite, ExampleTest) {
-    EXPECT_EQ(add(1, 2), 3);
-}
 
 int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+   std::cout<<"\n\n\n\n hello world"<<std::endl;
+   return 0;
 }
