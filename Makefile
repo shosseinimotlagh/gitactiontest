1eca8acb4e37d3e9637e8915505f81cafeb86c95
shosseinimotlagh <shosseinimotlagh@ebay.com>
2024-03-01 14:06:09 -0800

    sleep

--- Makefile
+++ Makefile
@@ -10,5 +10,8 @@ main.o: main.cpp
 check: main
 	./main
 
+distcheck: check
+ # Add commands to check the distribution here
+
 clean:
 	rm -f main.o main
--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
-
-
+#include <thread>
+#include <chrono>
 
 int main(int argc, char **argv) {
-   std::cout<<"\n\n\n\n hello world"<<std::endl;
-   return 0;
+    std::cout<<"\n\n\n\n hello world"<<std::endl;
+
+    // Sleep for 10 minutes
+    std::this_thread::sleep_for(std::chrono::minutes(10));
+
+    return 0;
 }
