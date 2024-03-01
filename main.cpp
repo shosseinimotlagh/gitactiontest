714d10a3b7940c6da90b0379a6c297703d4daf42
Mehdi Hosseini <116847813+shosseinimotlagh@users.noreply.github.com>
2024-03-01 14:49:17 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
-#include <thread>
-#include <chrono>
+
 
 int main(int argc, char **argv) {
     std::cout<<"\n\n\n\n hello world"<<std::endl;
 
-    // Sleep for 10 minutes
-    std::this_thread::sleep_for(std::chrono::minutes(10));
+
 
     return 0;
 }
